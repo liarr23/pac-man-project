0bf5015273032981e29942a43acdaf31f11ad422
91MrX <d2606107871@163.com>
2026-04-29 20:31:48 +0800

    环境配置

---
+++ src/Character.h
@@ -0,0 +1,15 @@
+//
+// Created by MrX on 2026/4/29.
+//
+
+#ifndef PACMAN_CHARACTER_H
+#define PACMAN_CHARACTER_H
+#include "GameObject.h"
+
+
+class Character:GameObject {
+};
+
+
+
+#endif //PACMAN_CHARACTER_H
