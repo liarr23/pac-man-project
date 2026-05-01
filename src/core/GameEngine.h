c0bfceb575574db7aaa53f53c1c236f2bc7a6d07
liarr23 <xzx070323@gmail.com>
2026-05-01 23:05:10 +0800

    架构文档

--- src/core/GameEngine.h
+++ src/core/GameEngine.h
@@ -6,12 +6,12 @@ class GameEngine {
 public:
     GameEngine();
     ~GameEngine();
-    InputHandler m_inputHandler;
-    void run();
+    void run();// 游戏主循环
     private:
-    void processEvents();
-    void update(sf::Time deltaTime);
-    void render();
-    sf ::RenderWindow window;
-    sf ::Clock clock;
+    InputHandler m_inputHandler;// 输入处理器
+    void processEvents();// 处理输入事件
+    void update(sf::Time deltaTime);// 更新游戏状态
+    void render();// 渲染游戏
+    sf ::RenderWindow window;// 游戏窗口
+    sf ::Clock clock;// 游戏时钟
 };
